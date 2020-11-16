442ac2c03e6649876644a84989eefc65cac22dd5
Ebin Paul <ebinpaul@outlook.com>
2020-11-16 23:04:15 +0530

    Update urls.py

--- to_do/to_do/urls.py
+++ to_do/to_do/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.HomePage.as_view(), name='home'),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
