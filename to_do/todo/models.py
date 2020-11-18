e2acdab8e11c6d334fa486c3f7030e5fe8237bfd
Ebin Paul <ebinpaul@outlook.com>
2020-11-18 19:36:56 +0530

    Update models.py

--- to_do/todo/models.py
+++ to_do/todo/models.py
@@ -1,3 +1,15 @@
+from django.db.models.deletion import CASCADE
+from to_do.accounts.models import User
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, related_name='todos', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.title
