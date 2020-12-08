6a465fe5f1df1ee630fc9e10df2deeb2a544236c
Ebin Paul <ebinpaul@outlook.com>
2020-12-08 14:54:38 +0530

    Update api-service.js

--- to_do_frontend/src/api-service.js
+++ to_do_frontend/src/api-service.js
@@ -24,4 +24,37 @@ export class API{
             }
         ).then( resp => resp.json())       
     }
+
+    static getTasks(token){
+        return fetch(
+            "http://127.0.0.1:8000/api/task/",
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`
+                }
+            }
+        ).then(resp => resp.json())
+    }
+
+    static deleteTask(task_id, token){
+        return fetch(`http://127.0.0.1:8000/api/task/${task_id}/`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            }})
+    }
+
+    static addTask(body, token){
+        return fetch(`http://127.0.0.1:8000/api/movies/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json())
+    }
 }
