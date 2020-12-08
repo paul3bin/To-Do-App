eed812a8675630d3c140c0f1d8634101b83086b9
Ebin Paul <ebinpaul@outlook.com>
2020-12-08 14:54:49 +0530

    Update App.js

--- to_do_frontend/src/App.js
+++ to_do_frontend/src/App.js
@@ -4,9 +4,15 @@ import { useCookies } from 'react-cookie';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
+import { TasksList } from './components/list-tasks';
+import {API} from './api-service'
+
 function App() {
   
   const [token, setToken, deleteToken] = useCookies(['token']);
+
+  const [tasks, setTasks] = useState([]);
+
   
   useEffect(() => {
     if(!token['token']) window.location.href = '/'
@@ -15,13 +21,29 @@ function App() {
   const logoutUser = () => {
     deleteToken(['token']);
   }
+
+  useEffect(() => {
+    API.getTasks(token['token']).then(resp => setTasks(resp))
+  }, [])
+
+  const updateTaskListAction = task =>{
+    const updatedTaskList = tasks.filter(tsk => tsk.id !== task.id)
+    setTasks(updatedTaskList)
+  }
   
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>To-Do Tasks</h2>
+      <nav className="navbar navbar-dark bg-dark">
+        <h2>Tasks To-Do</h2>
         <FontAwesomeIcon icon={faSignOutAlt} className="Icons" onClick={logoutUser}/>
-      </header>
+      </nav>
+      <div>
+        <TasksList 
+          tasks={tasks}
+          updateTaskList={updateTaskListAction}
+        />
+        <button className='btn btn-success'>Add New Task</button>
+      </div>
     </div>
   );
 }
