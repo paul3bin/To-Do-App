702010ba630ecdd8e5745f5dea32cf631fe45d9c
Ebin Paul <ebinpaul@outlook.com>
2021-01-24 13:08:29 +0530

    Update urls.py

--- to_do_backend/to_do_backend/urls.py
+++ to_do_backend/to_do_backend/urls.py
@@ -21,5 +21,4 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('to_do_api.urls')),
-    path('auth/', obtain_auth_token),
 ]
