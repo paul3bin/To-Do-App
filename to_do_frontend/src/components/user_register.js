ca0fdad9fa8fe2c2d3e46ab9abddae2c21a5df41
Ebin Paul <ebinpaul@outlook.com>
2021-01-28 13:16:04 +0530

    Update user_register.js

--- to_do_frontend/src/components/user_register.js
+++ to_do_frontend/src/components/user_register.js
@@ -11,7 +11,17 @@ function RegisterUser(){
     
     const [password, setPassword] = useState('');
 
-    const isDisabled = username.length===0||password.length===0;
+    const [passwordReEnter, setPasswordReEnter] = useState('');
+
+    const isDisabled = username.length===0||password.length===0 || passwordReEnter.length===0;
+
+    const passwordCheck = () =>{
+        return (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password))
+    }
+
+    const passwordReEnterCheck = () =>{
+        return (password.length===passwordReEnter.length)
+    }
 
     const registerClicked = () => {
         API.registerUser({username, password})
@@ -25,17 +35,21 @@ function RegisterUser(){
             <header className='App-header'>
                 <h1>Register</h1>
                 <div class="mb-3">
-                    <input id='UserName' type='text' className="form-control" placeholder='Username' value={username} 
+                    <input id='UserName' type='text' className="form-control" placeholder='Enter Username' value={username} 
                         onChange={evnt => setUsername(evnt.target.value)}/>
                 </div>
                 <div class="mb-3">
-                    <input id='password' type='password' className="form-control" placeholder='Password' value={password} 
-                        onChange={evnt => setPassword(evnt.target.value)} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-                        title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+                    <input id='password' type='password' className="form-control" placeholder='Enter Password' value={password} 
+                        onChange={evnt => setPassword(evnt.target.value)}/>
+                </div>
+
+                <div class="mb-3">
+                    <input id='password_reEnter' type='password' className="form-control" placeholder='Re-enter Password' value={passwordReEnter} 
+                        onChange={evnt => setPasswordReEnter(evnt.target.value)}/>
                 </div>
 
                 <button disabled={isDisabled} className='btn btn-outline-primary Login-items' 
-                onClick={registerClicked}>Login</button>
+                onClick={registerClicked}>Create Account</button>
 
                 <p className='p-login-register'>Already have an account? Login <Link to="/">here</Link>.</p> 
             </header>
